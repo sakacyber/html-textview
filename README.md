1aad9eb813e1c427f58dfe84ec1c3f58607d40da
Saka Gamer <chansakleng007@gmail.com>
2020-05-22 10:19:32 +0700

    update to v1.0.6

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ It currently supports tags </>:
 Add dependency:
 
 ```
-implementation 'com.github.SakaGamer:html-textview:1.0.5'
+implementation 'com.github.SakaGamer:html-textview:1.0.6'
 ```
 
 ## Usage
--- html-textview/src/main/java/com/saka/android/htmltextview/ImageView.kt
+++ html-textview/src/main/java/com/saka/android/htmltextview/ImageView.kt
@@ -1,11 +1,7 @@
 package com.saka.android.htmltextview
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Path
-import android.graphics.RectF
 import android.util.AttributeSet
-import android.view.View
 
 class ImageView @JvmOverloads constructor(
     context: Context,
@@ -13,36 +9,23 @@ class ImageView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : BaseElement(context, attrs, defStyleAttr) {
 
-    private var radius = DEFAULT_CORNER
-    private var rect: RectF = RectF()
-    private var path: Path = Path()
-
-    override fun onDraw(canvas: Canvas?) {
-        rect.set(0f, 0f, width.toFloat(), height.toFloat())
-        path.addRoundRect(rect, radius, radius, Path.Direction.CW)
-        canvas?.clipPath(path)
-        super.onDraw(canvas)
-    }
-
     override fun render() {
         orientation = VERTICAL
-        layoutParams = LayoutParams(
-            LayoutParams.MATCH_PARENT,
-            LayoutParams.WRAP_CONTENT
-        )
         setImage()
     }
 
     private fun setImage() {
-        val imageView = View.inflate(context, R.layout.image_view, null) as RoundImageView
-        imageView.load(link)
-        addView(imageView)
+        val roundImageView = RoundImageView(context)
+        roundImageView.adjustViewBounds = true
+        roundImageView.load(link)
+        val layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(0, 18, 0, 18)
+        addView(roundImageView, layoutParams)
     }
 
     private val link: String?
         get() = element.attr("abs:src")
-
-    companion object {
-        private const val DEFAULT_CORNER = 18F
-    }
 }
